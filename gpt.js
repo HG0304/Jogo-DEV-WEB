340f1589cbd0ae6cfe8d92fd48437fe4515df240
Hugo <hugoen304@gmail.com>
2023-05-19 13:51:18 -0300

    gpt burro

--- gpt.js
+++ gpt.js
@@ -1,6 +1,6 @@
 // Obtém o elemento canvas
 /** @type {HTMLCanvasElement} */
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 
 // Array para armazenar as balas
@@ -11,39 +11,29 @@ function createBullet() {
     // Gera um número aleatório entre 0 e 3
     var side = Math.floor(Math.random() * 4);
 
-    // Posição e velocidade iniciais
-    var x, y, dx, dy;
-    var speed = 2;
-
+    // Posição inicial nas extremidades
+    var x, y;
     switch (side) {
         case 0: // Topo do mapa
             x = Math.random() * canvas.width;
             y = 0;
-            dx = 0;
-            dy = speed;
             break;
         case 1: // Lateral direita do mapa
             x = canvas.width;
             y = Math.random() * canvas.height;
-            dx = -speed;
-            dy = 0;
             break;
         case 2: // Base do mapa
             x = Math.random() * canvas.width;
             y = canvas.height;
-            dx = 0;
-            dy = -speed;
             break;
         case 3: // Lateral esquerda do mapa
             x = 0;
             y = Math.random() * canvas.height;
-            dx = speed;
-            dy = 0;
             break;
     }
 
     // Adiciona a bala ao array
-    bullets.push({ x: x, y: y, dx: dx, dy: dy });
+    bullets.push({ x: x, y: y });
 }
 
 // Função para atualizar a posição das balas
@@ -51,14 +41,10 @@ function updateBullets() {
     for (var i = 0; i < bullets.length; i++) {
         var bullet = bullets[i];
 
-        // Atualiza a posição da bala
-        bullet.x += bullet.dx;
-        bullet.y += bullet.dy;
-
-        // Verifica se a bala saiu do canvas e remove-a do array
-        if (bullet.x < 0 || bullet.x > canvas.width || bullet.y < 0 || bullet.y > canvas.height) {
-            bullets.splice(i, 1);
-            i--;
+        // Movimento em linha reta
+        if (bullet.x < canvas.width && bullet.y < canvas.height) {
+            bullet.x += 2; // Velocidade horizontal
+            bullet.y += 2; // Velocidade vertical
         }
     }
 }
@@ -91,5 +77,3 @@ function gameLoop() {
 window.addEventListener("load", function() {
     gameLoop();
 });
-
-gameLoop();
